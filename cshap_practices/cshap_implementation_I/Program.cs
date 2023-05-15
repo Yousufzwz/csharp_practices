0ca2059bed39fee6174e40436c6c94f1efa50f79
Yusuf-Zw <yousufzwz@gmail.com>
2023-05-15 17:08:06 +0600

    first practiced code committed

---
+++ cshap_practices/cshap_implementation_I/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string name = "MahmudulHasan";
+int nameSize = name.Length;
+Console.WriteLine(nameSize);
+
+string firstName = "Ahmad";
+string lastName = "Esa";
+string fullName= firstName + " "+ lastName;
+Console.WriteLine(fullName);
+
+string newString = "Good Boy";
+string subNewString = newString.Substring(5, 3);
+Console.WriteLine(subNewString);
+
+int newNum = 100;
+string convertedNum=newNum.ToString();
+Console.WriteLine(convertedNum);
+
+
