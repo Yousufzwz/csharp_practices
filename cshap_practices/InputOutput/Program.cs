25d58233d0d12365624346d89423350a226f3f63
Yusuf-Zw <yousufzwz@gmail.com>
2023-05-18 17:41:15 +0600

    practiced code committed v5

--- cshap_practices/InputOutput/Program.cs
+++ cshap_practices/InputOutput/Program.cs
@@ -12,7 +12,7 @@ string Input= Console.ReadLine();
 string[] dataInput = Input.Split(',');
 
 Console.WriteLine($"Name: {dataInput[0]}, Age: {dataInput[1]}, Occupation: {dataInput[2]}, Hobby: {dataInput[3]}");
-*/
+
 
 
 string newInput=Console.ReadLine();
@@ -20,5 +20,19 @@ string newInput=Console.ReadLine();
 var newData = newInput.Split(';');
 
 Console.WriteLine($"Name: {newData[0]}, Age: {newData[1]}, Occupation: {newData[2]}, Hobby: {newData[3]}, Siblings: {newData[4]}");
+*/
 
 
+
+ConsoleKeyInfo input = Console.ReadKey();
+Console.WriteLine("a");   
+int a;
+
+
+var numInput = Console.ReadLine();
+
+var num1= int.Parse(numInput);
+numInput = Console.ReadLine();  
+var num2= int.Parse(numInput);
+var sum = num1 * num2;
+Console.WriteLine(sum);
