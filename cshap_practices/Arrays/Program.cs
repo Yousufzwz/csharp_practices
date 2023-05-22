d8cf05ce6c54a23b3e2b15121d6cd913b6942c9f
Yusuf-Zw <yousufzwz@gmail.com>
2023-05-22 13:47:01 +0600

    practiced code committed v9

--- cshap_practices/Arrays/Program.cs
+++ cshap_practices/Arrays/Program.cs
@@ -148,6 +148,271 @@ tokens[1]=tokenList2;
 tokens[2]=tokenList3;
 
 
+/*
+int[][,] newArray = new int[][,]
+{
+    new int[,] { {89, 10}, {100, 90} },
+    new int[,] { {20, 5}, {11, 12}, {18, 19} },
+    new int[,] { {21, 22}, {23, 24}, {25, 26} }
+};
+
+// Iterate through each element in the array
+for (int i = 0; i < newArray.Length; i++)
+{
+    int[,] subArray = newArray[i];
+
+    // Iterate through each element in the subarray
+    for (int j = 0; j < subArray.GetLength(0); j++)
+    {
+        for (int k = 0; k < subArray.GetLength(1); k++)
+        {
+            int element = subArray[j, k];
+            Console.WriteLine(element);
+        }
+    }
+}
+*/
+
+/*
+int[][] newArray2 = new int[3][];
+newArray2[0] = new int[] {1,2};
+newArray2[1] = new int[] { 1, 2, 3, 4, 5 };
+newArray2[2] = new int[] { 1 };
+
+
+
+int[][][] newArray3 = new int[3][][];
+
+newArray3[0] = new int[2][];         
+newArray3[1] = new int[3][];        
+newArray3[2] = new int[1][];        
+
+newArray3[0][0] = new int[3];       
+newArray3[0][1] = new int[2];       
+newArray3[1][0] = new int[4];       
+newArray3[1][1] = new int[2];       
+newArray3[1][2] = new int[3]; 
+newArray3[2][0] = new int[2];       
+
+newArray3[0][0][0] = 1;             
+int value = newArray3[0][0][0];     
+
+
+
+int[,] maleTeachers = //new int [2,3]
+{ {10, 11, 12}, {13, 14, 15 } };
+
+
+for(int mt=0; mt<maleTeachers.GetLength(0); mt++)
+{
+    for(int mt1=0; mt1<maleTeachers.GetLength(1); mt1++)
+    {
+        Console.WriteLine(maleTeachers[mt, mt1]);
+    }
+}
+
+
+int[,,,] studentID = { //[3,1,1,5]
+    { { { 301, 302, 303, 304, 305 } } },
+    { { { 306, 307, 308, 309, 310 } }},
+    {{ { 50, 51, 52, 53, 54} } }
+   
+
+};
+    
+
+for (int i = 0; i < studentID.GetLength(0); i++)
+{
+    for (int j = 0; j < studentID.GetLength(1); j++)
+    {
+       for(int k = 0; k < studentID.GetLength(2); k++)
+        {
+          for(int m =0;  m < studentID.GetLength(3); m++)
+            {
+                Console.WriteLine(studentID[i, j, k, m]);
+            }
+        }
+    }
+}
+
+*/
+/*
+int dataRow = int.Parse(Console.ReadLine());
+
+int[][] dataArray = new int[dataRow][]; 
+
+for (int i = 0; i < dataRow; i++)
+{
+    var inputtingNums = Console.ReadLine().Split(' ');
+    dataArray[i] = new int[inputtingNums.Length];
+    
+    for(int j = 0;j < inputtingNums.Length; j++)
+    { dataArray[i][j] = int.Parse(inputtingNums[j]);}
+}
+
+*/
+
+
+
+
+//int[,] matrix = {
+//    {1, 2, 3},
+//    {4, 5, 6},
+//    {7, 8, 9}
+//};
+
+
+
+//for (int i = 0; i < matrix.GetLength(0); i++)
+//{
+//    for (int j = i + 1; j < matrix.GetLength(1); j++)
+//    {
+//        int temp = matrix[i, j];
+//        matrix[i, j] = matrix[j, i];
+//        matrix[j, i] = temp;
+//    }
+//}
+
+
+//for (int i = 0; i < matrix.GetLength(1); i++)
+//{
+//    for (int j = 0; j < matrix.GetLength(0); j++)
+//    {
+//        Console.Write(matrix[i, j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+//int[,] student = { { 10, 20}, { 30, 40} };
+
+//for(int i = 0; i < student.GetLength(0);i++)
+//{
+//    for(int j =0; j < student.GetLength(1); j++)
+//    {
+//        Console.WriteLine(student[i,j]);
+
+//    }
+//}
+
+//int[][] new3d = new int[3][];
+
+
+
+
+
+//student data input-output
+/*
+var num1 = int.Parse(Console.ReadLine());
+var num2 = int.Parse(Console.ReadLine());
+
+string[,] studentData = new string[num1, num2];
+
+for (int i = 0; i < num1; i++)
+{
+    Console.WriteLine($"Enter the name and ID of student {i + 1}:");
+    string input = Console.ReadLine();
+    string[] data = input.Split(',');
+
+    studentData[i, 0] = data[0];  
+    studentData[i, 1] = data[1];  
+}
+
+for (int i = 0; i < num1; i++)
+{
+    string name = studentData[i, 0];
+    string id = studentData[i, 1];
+    Console.WriteLine($"{name} {id}");
+}
+*/
+/*
+string input;
+while (true)
+{
+    Console.WriteLine("Enter a string (or 'end' to exit):");
+    input = Console.ReadLine();
+
+    if (input.ToLower() == "end")
+        break;
+
+    int sum = 0;
+    foreach (char c in input)
+    {
+        sum += c;
+    }
+
+    Console.WriteLine($"Sum of ASCII values: {sum}");
+}
+*/
+
+
+
+/*
+// Define the jagged array
+int[][] jaggedArray = new int[3][];
+
+// Input and store data in the jagged array
+for (int i = 0; i < jaggedArray.Length; i++)
+{
+    Console.WriteLine($"Enter the number of elements for array {i + 1}:");
+    int size = int.Parse(Console.ReadLine());
+
+    jaggedArray[i] = new int[size];
+
+    Console.WriteLine($"Enter {size} elements for array {i + 1}:");
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        jaggedArray[i][j] = int.Parse(Console.ReadLine());
+    }
+}
+
+// Print the stored data
+Console.WriteLine("The stored data in the jagged array:");
+for (int i = 0; i < jaggedArray.Length; i++)
+{
+    Console.Write($"Array {i + 1}: ");
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        Console.Write(jaggedArray[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+*/
+
+int[][] employeeData = new int[3][];
+employeeData[0] = new int[] { 10, 20, 30, 40, 50 };
+employeeData[1] = new int[1] { 100 };
+employeeData[2] = new int[2] { 121, 131 };
+
+for (int i = 0; i < employeeData.Length; i++)
+{
+    for (int j = 0; j < employeeData[i].Length; j++)
+    {
+        Console.WriteLine(employeeData[i][j] + " ");
+    }
+}
+
+int[][,] officeData = new int[3][,];
+officeData[0] = new int[,] { { 10, 20 }, { 1, 2 } };
+officeData[1] = new int[,] { { 2, 3, 4, 5, 6, 7, 8 }, { 100, 200, 300, 400, 300, 400, 200 } };
+officeData[2] = new int[,] { { 1 } };
+
+
+for (int i = 0; i < officeData.Length; i++)
+{
+    Console.WriteLine($"Array {i + 1}:");
+    for (int j = 0; j < officeData[i].GetLength(0); j++)
+    {
+        for (int k = 0; k < officeData[i].GetLength(1); k++)
+        {
+            Console.Write(officeData[i][j, k] + " ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
 
--- cshap_practices/ControlStatements/Program.cs
+++ cshap_practices/ControlStatements/Program.cs
@@ -142,160 +142,6 @@
 #endregion
 
 
-/*
-int[][,] newArray = new int[][,]
-{
-    new int[,] { {89, 10}, {100, 90} },
-    new int[,] { {20, 5}, {11, 12}, {18, 19} },
-    new int[,] { {21, 22}, {23, 24}, {25, 26} }
-};
-
-// Iterate through each element in the array
-for (int i = 0; i < newArray.Length; i++)
-{
-    int[,] subArray = newArray[i];
-
-    // Iterate through each element in the subarray
-    for (int j = 0; j < subArray.GetLength(0); j++)
-    {
-        for (int k = 0; k < subArray.GetLength(1); k++)
-        {
-            int element = subArray[j, k];
-            Console.WriteLine(element);
-        }
-    }
-}
-*/
-
-/*
-int[][] newArray2 = new int[3][];
-newArray2[0] = new int[] {1,2};
-newArray2[1] = new int[] { 1, 2, 3, 4, 5 };
-newArray2[2] = new int[] { 1 };
-
-
-
-int[][][] newArray3 = new int[3][][];
-
-newArray3[0] = new int[2][];         
-newArray3[1] = new int[3][];        
-newArray3[2] = new int[1][];        
-
-newArray3[0][0] = new int[3];       
-newArray3[0][1] = new int[2];       
-newArray3[1][0] = new int[4];       
-newArray3[1][1] = new int[2];       
-newArray3[1][2] = new int[3]; 
-newArray3[2][0] = new int[2];       
-
-newArray3[0][0][0] = 1;             
-int value = newArray3[0][0][0];     
-
-
-
-int[,] maleTeachers = //new int [2,3]
-{ {10, 11, 12}, {13, 14, 15 } };
-
-
-for(int mt=0; mt<maleTeachers.GetLength(0); mt++)
-{
-    for(int mt1=0; mt1<maleTeachers.GetLength(1); mt1++)
-    {
-        Console.WriteLine(maleTeachers[mt, mt1]);
-    }
-}
-
-
-int[,,,] studentID = { //[3,1,1,5]
-    { { { 301, 302, 303, 304, 305 } } },
-    { { { 306, 307, 308, 309, 310 } }},
-    {{ { 50, 51, 52, 53, 54} } }
-   
-
-};
-    
-
-for (int i = 0; i < studentID.GetLength(0); i++)
-{
-    for (int j = 0; j < studentID.GetLength(1); j++)
-    {
-       for(int k = 0; k < studentID.GetLength(2); k++)
-        {
-          for(int m =0;  m < studentID.GetLength(3); m++)
-            {
-                Console.WriteLine(studentID[i, j, k, m]);
-            }
-        }
-    }
-}
-
-*/
-/*
-int dataRow = int.Parse(Console.ReadLine());
-
-int[][] dataArray = new int[dataRow][]; 
-
-for (int i = 0; i < dataRow; i++)
-{
-    var inputtingNums = Console.ReadLine().Split(' ');
-    dataArray[i] = new int[inputtingNums.Length];
-    
-    for(int j = 0;j < inputtingNums.Length; j++)
-    { dataArray[i][j] = int.Parse(inputtingNums[j]);}
-}
-
-*/
-
-
-
-
-//int[,] matrix = {
-//    {1, 2, 3},
-//    {4, 5, 6},
-//    {7, 8, 9}
-//};
-
-
-
-//for (int i = 0; i < matrix.GetLength(0); i++)
-//{
-//    for (int j = i + 1; j < matrix.GetLength(1); j++)
-//    {
-//        int temp = matrix[i, j];
-//        matrix[i, j] = matrix[j, i];
-//        matrix[j, i] = temp;
-//    }
-//}
-
-
-//for (int i = 0; i < matrix.GetLength(1); i++)
-//{
-//    for (int j = 0; j < matrix.GetLength(0); j++)
-//    {
-//        Console.Write(matrix[i, j] + " ");
-//    }
-//    Console.WriteLine();
-//}
-
-
-
-
-
-//int[,] student = { { 10, 20}, { 30, 40} };
-
-//for(int i = 0; i < student.GetLength(0);i++)
-//{
-//    for(int j =0; j < student.GetLength(1); j++)
-//    {
-//        Console.WriteLine(student[i,j]);
-
-//    }
-//}
-
-//int[][] new3d = new int[3][];
-
-
-
 //string names = "mahmudul,hasan,rakib,uddin,ahmad,esa,abu,sufian,zakariya";
 //var people = names.Split(',');
 
@@ -365,79 +211,20 @@ Console.WriteLine(lastPart);
 
 */
 
-//student data input-output
-/*
-var num1 = int.Parse(Console.ReadLine());
-var num2 = int.Parse(Console.ReadLine());
 
-string[,] studentData = new string[num1, num2];
 
-for (int i = 0; i < num1; i++)
+
+using System.Text;
+
+var message = "adfjlkdsfjlasdkjffjlfffoidffffflllaffffffahihdsfa";
+StringBuilder seletedMessage = new StringBuilder(message);
+for(int i=0; i<message.Length; i++)
 {
-    Console.WriteLine($"Enter the name and ID of student {i + 1}:");
-    string input = Console.ReadLine();
-    string[] data = input.Split(',');
-
-    studentData[i, 0] = data[0];  
-    studentData[i, 1] = data[1];  
+    if (seletedMessage[i] =='f')
+    
+        seletedMessage[i] = 'W';
+    
+  
 }
-
-for (int i = 0; i < num1; i++)
-{
-    string name = studentData[i, 0];
-    string id = studentData[i, 1];
-    Console.WriteLine($"{name} {id}");
-}
-*/
-/*
-string input;
-while (true)
-{
-    Console.WriteLine("Enter a string (or 'end' to exit):");
-    input = Console.ReadLine();
-
-    if (input.ToLower() == "end")
-        break;
-
-    int sum = 0;
-    foreach (char c in input)
-    {
-        sum += c;
-    }
-
-    Console.WriteLine($"Sum of ASCII values: {sum}");
-}
-*/
-
-
-
-// Define the jagged array
-int[][] jaggedArray = new int[3][];
-
-// Input and store data in the jagged array
-for (int i = 0; i < jaggedArray.Length; i++)
-{
-    Console.WriteLine($"Enter the number of elements for array {i + 1}:");
-    int size = int.Parse(Console.ReadLine());
-
-    jaggedArray[i] = new int[size];
-
-    Console.WriteLine($"Enter {size} elements for array {i + 1}:");
-    for (int j = 0; j < jaggedArray[i].Length; j++)
-    {
-        jaggedArray[i][j] = int.Parse(Console.ReadLine());
-    }
-}
-
-// Print the stored data
-Console.WriteLine("The stored data in the jagged array:");
-for (int i = 0; i < jaggedArray.Length; i++)
-{
-    Console.Write($"Array {i + 1}: ");
-    for (int j = 0; j < jaggedArray[i].Length; j++)
-    {
-        Console.Write(jaggedArray[i][j] + " ");
-    }
-    Console.WriteLine();
-}
-
+message = seletedMessage.ToString();
+Console.WriteLine(message);
