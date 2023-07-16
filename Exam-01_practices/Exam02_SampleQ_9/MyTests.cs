c5f062c105127121859273b5a8fb743d9893a569
Yusuf-Zw <yousufzwz@gmail.com>
2023-07-16 22:32:51 +0600

    practiced code committed v.2

---
+++ Exam-01_practices/Exam02_SampleQ_9/MyTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam02_SampleQ_9;
+
+internal class MyTests<T1, T2>
+{
+    private (T1, T2) sampleTuple;
+
+    public MyTests(T1 name, T2 age)
+    {
+        sampleTuple=(name, age);
+    }
+
+    public void PrintTuple()
+    {
+        Console.WriteLine(sampleTuple.Item1);
+        Console.WriteLine(sampleTuple.Item2);
+
+    }
+}
---
+++ Exam-01_practices/Exam02_SampleQ_9/Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+using Exam02_SampleQ_9;
+using System.Collections;
+
+string txt = "This.is!a,sample text";
+string[] txtWords=txt.Split(new char[] {' ', '.', ',', '?', '!'});
+
+Dictionary<string, int> wordOccurences = new Dictionary<string, int>();  
+foreach (string word in txtWords)
+{
+    if(wordOccurences.ContainsKey(word))
+    {
+        wordOccurences[word]++;
+    }
+
+    else
+    {
+        wordOccurences.Add(word, 1);
+    }
+}
+
+foreach(KeyValuePair<string, int> pair in wordOccurences)
+{
+    Console.WriteLine("Word:{0}, Occurences:{1}", pair, pair.Value);
+}
+
+Console.WriteLine();
+
+Hashtable personData= new Hashtable();
+personData.Add("Name", "Esa");
+personData.Add("Age", "34");
+
+foreach(var key in personData.Keys)
+{
+    Console.WriteLine("Key :"+ key );
+    Console.WriteLine("Value :"+ personData[key]);
+}
+
+Console.WriteLine();
+
+List<string> fruits= new List<string>();
+fruits.Add("Banana");
+fruits.Add("Mango");
+fruits.Add("Pineapple");
+
+Console.WriteLine(fruits[0]);
+Console.WriteLine(fruits[1]);
+Console.WriteLine(fruits[2]);
+
+
+fruits[1] = "Watermelon";
+fruits.RemoveAt(0);
+
+foreach(var fru in fruits)
+{
+    Console.WriteLine(fru);
+}
+
+bool containsPA = fruits.Contains("Pineapple");
+Console.WriteLine(containsPA);
+
+int counter=fruits.Count;
+Console.WriteLine(counter);
+
+
+Console.WriteLine();
+
+ArrayList numbers= new ArrayList();
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+
+numbers[2] = 1000;
+numbers.Remove(1);
+
+foreach(var n in numbers)
+{
+    Console.WriteLine(n);
+}
+
+bool contains30 = numbers.Contains(300);
+Console.WriteLine(contains30);  
+
+int count = numbers.Count;
+Console.WriteLine(count);  
+
+numbers.Clear();
+
+count = numbers.Count;
+Console.WriteLine(count);
+
+
+Console.WriteLine();
+var person = GetPerson();
+Console.WriteLine(person.name);
+Console.WriteLine(person.country);
+
+(string name, int age, string country)GetPerson()
+{
+    return ("Mahmud", 29, "Bangladesh");
+}
+
+
+Console.WriteLine();
+
+MyTests<string, int> sampleData = new MyTests<string, int>("Zakariya", 29);
+sampleData.PrintTuple();
