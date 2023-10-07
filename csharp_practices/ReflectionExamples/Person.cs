afdf4f8fe7ec74965efb8b27e1ded83c1dcd6cde
Yusuf-Zw <yousufzwz@gmail.com>
2023-10-07 16:31:47 +0600

    practiced code committed v.1

---
+++ csharp_practices/ReflectionExamples/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExamples;
+
+public class Person : ICloneable
+{
+    private int Id { get; set; }    
+    private string Name { get; set; }
+
+    public void Walk(int speed)
+    {
+        Console.WriteLine("Walking");
+    }
+
+    private void Talk(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public object Clone()
+    {
+        return new Person { Id =Id, Name=Name};
+    }
+}
---
+++ csharp_practices/ReflectionExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using ReflectionExamples;
+
+Type t = typeof(Person);
+
+Person p = new ();
+Type type = p.GetType ();
+
+MethodInfo [] methods= t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+foreach(MethodInfo m in methods)
+{
+    Console.WriteLine(m.Name);
+}
+
+ConstructorInfo defaultConstructor = t.GetConstructor(new Type[] { });
+Person person = (Person)defaultConstructor.Invoke(new object[] { });
+
+MethodInfo talkMethod= t.GetMethod("Talk",
+    BindingFlags.Instance | BindingFlags.NonPublic, new Type[] {typeof(string)});
+
+talkMethod.Invoke(person, new object[] { "Assalamualaikum" });    
